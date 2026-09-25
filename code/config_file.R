f0babeb0a505e97c93a8e8281808cb90e51623ba
ScarySyrup <joetarr06@gmail.com>
2026-09-25 14:47:07 +0100

    Add files via upload

--- code/config_file.R
+++ code/config_file.R
@@ -1,11 +1,12 @@
 #options(error = NULL)
 options(scipen=999)
+
 JONES_ACT <<- TRUE
 
 
 Shock_rail <<- FALSE
-Shock_ships <<- TRUE
-Shock_pipes <<- FALSE
+Shock_ships <<- FALSE
+Shock_pipes <<- TRUE
 
 
 
@@ -14,3 +15,6 @@ Include_multi_mode <<- FALSE
 options(scipen=999)
 
 parent_direct <- "/Users/josephtarr/Documents/allfed/refactored/refactored_allfed/"
+
+#This runs the model for each state
+source(paste(parent_direct,"code/master.R",sep=""),local=.GlobalEnv)
